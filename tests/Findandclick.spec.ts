cddf11d3f746b52b817600c63d6369a5c0d4bd76
MonalishaSahoo03 <monalisha.sahoo@sutara.org>
2025-12-17 15:24:21 +0530

    click on particular item

--- tests/Findandclick.spec.ts
+++ tests/Findandclick.spec.ts
@@ -4,7 +4,8 @@ test('find a product from flipkart and click on it',async({page})=>{
      const fash = await page.locator("//div[@aria-label='Fashion']");
      await fash.hover();
 
-     await page.locator("//a[contains(text(),'Men Footwear')]").click();
+     const men = await page.locator("//a[contains(text(),'Men Footwear')]");
+      await men.click();
 
      
      
