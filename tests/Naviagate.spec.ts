e354e8015f643ffbc7f62fd1c4dcad52f4f4a13f
MonalishaSahoo03 <monalisha.sahoo@sutara.org>
2025-12-17 11:36:07 +0530

    Add new Playwright test and update workflow

---
+++ tests/Naviagate.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('login to tangerin', async ({ page }) => {
+  await page.goto('https://tangerinestaging.ustadmobile.com/#/login');
+  await page.locator("username").fill('user1');
+  await page.locator('password').fill('password');
+  await page.locator("//span[contains(text(),'LOGIN')]").click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Tangerine Editor/);
+});
