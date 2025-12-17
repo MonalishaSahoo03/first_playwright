7496a1b71135e01fe0157f01a0da97a294b210f5
MonalishaSahoo03 <monalisha.sahoo@sutara.org>
2025-12-17 15:12:42 +0530

    click on one item from flipcart

---
+++ tests/Findandclick.spec.ts
@@ -0,0 +1,12 @@
+import{test,expect} from '@playwright/test'
+test('find a product from flipkart and click on it',async({page})=>{
+    await page.goto("https://www.flipkart.com/");
+     const fash = await page.locator("//div[@aria-label='Fashion']");
+     await fash.hover();
+
+     await page.locator("//a[contains(text(),'Men Footwear')]").click();
+
+     
+     
+
+})
